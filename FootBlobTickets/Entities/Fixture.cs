ff0b2c19de0a3d0b1bf52e1f14f66f267b226eed
Zebseb <113332074+Zebseb@users.noreply.github.com>
2024-01-23 13:45:06 +0100

    Added fixture class and ticketscontroler

---
+++ FootBlobTickets/Entities/Fixture.cs
@@ -0,0 +1,11 @@
+﻿namespace FootBlobTickets.Entities
+{
+	public class Fixture
+	{
+        public Guid FixtureId { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public int StadiumCapacity { get; set; }
+        public int TicketsSold { get; set; }
+    }
+}
