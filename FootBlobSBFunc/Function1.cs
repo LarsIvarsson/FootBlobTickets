7fbbac966a92bc73e4ef9b5e19fae9459cc26dd4
Lars Ivarsson <lars.ivarsson@gmail.com>
2024-02-05 15:00:31 +0100

    added sb triggered azure function

---
+++ FootBlobSBFunc/Function1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FootBlobSBFunc
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(Function1))]
+        public async Task Run(
+            [ServiceBusTrigger("sbt-tickettopic", "sbs-ticketSubscription", Connection = "sbConnString")]
+            ServiceBusReceivedMessage message,
+            ServiceBusMessageActions messageActions)
+        {
+            _logger.LogInformation("Message ID: {id}", message.MessageId);
+            _logger.LogInformation("Message Body: {body}", message.Body);
+            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+
+             // Complete the message
+            await messageActions.CompleteMessageAsync(message);
+        }
+    }
+}
