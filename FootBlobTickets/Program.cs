547102cf95471f916121bd13bc86ee76de6e793d
Lars Ivarsson <lars.ivarsson@gmail.com>
2024-02-12 10:34:42 +0100

    added cors allowance in web api

--- FootBlobTickets/Program.cs
+++ FootBlobTickets/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(); 
 
 var app = builder.Build();
 
@@ -16,6 +17,13 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin();
+    builder.AllowAnyMethod();
+    builder.AllowAnyHeader();
+});
 
 app.MapControllers();
 
